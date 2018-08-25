6563324921cc7f0a50d08bc2bb8ed59e8137e22c
BS <bs@BSs-Mac-Pro.local>
2018-08-25 21:58:19 +0200

    add multiple func

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Toggle from './RpcToggle'
+import Portal from './Portal'
 
 
 class Person extends Component {
@@ -24,6 +26,21 @@ export default class App extends Component {
       <div>
         <Person name={this.state.name}
         age={this.state.age}/>
+          <Toggle>
+            {({on, toggle}) => (
+            <Fragment>
+              { on && <h1> Helloooo </h1> }
+              <button onClick={toggle}>On / Off</button> 
+            <Portal>
+              {on && <h1>Hello, this is the portal</h1>}
+            </Portal>
+            </Fragment>
+          )}
+          </Toggle>
+
+          <Portal>
+            <p>Culpa ullamco non deserunt officia. Proident sunt sit elit laborum. Id id magna irure fugiat id exercitation quis culpa voluptate qui aute consequat tempor. Tempor deserunt eu cupidatat amet eu culpa Lorem dolor eu dolore culpa ut ipsum nulla. Ullamco commodo voluptate incididunt quis exercitation.</p>
+          </Portal>
       </div>
     )
   }
