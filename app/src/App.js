5e2636f22668433e126e2fbf8d0c9d32fe28bde3
BS <bs@BSs-Mac-Pro.local>
2018-08-26 14:32:54 +0200

    add the context api

--- app/src/App.js
+++ app/src/App.js
@@ -3,45 +3,82 @@ import Toggle from './RpcToggle'
 import Portal from './Portal'
 
 
-class Person extends Component {
-  render() {
-    let { name, age } = this.props
-    return (
-      <div>
-        <p>  Hello my name is: {name} and i am {age}</p>
-      </div>
-    )
-  }
-}
+/**
+ * How to work with the Context api
+ */
 
+// 1 - Make  a new context 
+const TheContext = React.createContext()
 
-export default class App extends Component {
+// 2 - Create a provider Component
+class TheProvider extends Component {
   state = {
     name: "Brahim",
     age: 41,
     cool: true
   }
+  render () {
+    const { name } = this.state
+    return (
+      <TheContext.Provider value={ 
+        {
+          state: this.state,
+          growAge: () => this.setState({
+            age: this.state.age + 1
+          })
+        }
+      }>
+        {this.props.children}
+      </TheContext.Provider>
+    )
+  }
+}
+
+
+
+class Person extends Component {
+
   render() {
     return (
-      <div>
-        <Person name={this.state.name}
-        age={this.state.age}/>
-          <Toggle>
-            {({on, toggle}) => (
-            <Fragment>
-              { on && <h1> Helloooo </h1> }
-              <button onClick={toggle}>On / Off</button> 
-            <Portal>
-              {on && <h1>Hello, this is the portal</h1>}
-            </Portal>
-            </Fragment>
-          )}
-          </Toggle>
+      <div>  
+      <TheContext.Consumer>
+      { (value) => 
+      <Fragment>
+        <p>My name is {value.state.name} </p>
+        <p>My age is <span style={{color:"red", fontWeight: "bold"}}>{value.state.age}</span> </p>
+        <button onClick={value.growAge}>🎂</button>
+      </Fragment>
+      }
 
-          <Portal>
-            <p>Culpa ullamco non deserunt officia. Proident sunt sit elit laborum. Id id magna irure fugiat id exercitation quis culpa voluptate qui aute consequat tempor. Tempor deserunt eu cupidatat amet eu culpa Lorem dolor eu dolore culpa ut ipsum nulla. Ullamco commodo voluptate incididunt quis exercitation.</p>
-          </Portal>
-      </div>
+      </TheContext.Consumer>
+    </div>
+    )
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <TheProvider> 
+        <div>
+          <Person />
+            <Toggle>
+              {({on, toggle}) => (
+              <Fragment>
+                { on && <h1> Helloooo </h1> }
+                <button onClick={toggle}>On / Off</button> 
+              <Portal>
+                {on && <h1>Hello, this is the portal</h1>}
+              </Portal>
+              </Fragment>
+            )}
+            </Toggle>
+
+            <Portal>
+              <p>Culpa ullamco non deserunt officia. Proident sunt sit elit laborum. Id id magna irure fugiat id exercitation quis culpa voluptate qui aute consequat tempor. Tempor deserunt eu cupidatat amet eu culpa Lorem dolor eu dolore culpa ut ipsum nulla. Ullamco commodo voluptate incididunt quis exercitation.</p>
+            </Portal>
+        </div>
+      </TheProvider>
     )
   }
 }
