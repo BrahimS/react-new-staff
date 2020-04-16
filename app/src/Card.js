a6ed241c2895d57b6a4890dbc308d2a7421514b2
Brahim Sourny <bsourny@gmail.com>
2020-04-16 13:25:59 +0200

    add hooks test
    hooks

--- app/src/Card.js
+++ app/src/Card.js
@@ -1,1 +1,35 @@
+// React Hooks:
+// useState
+
+import React, { Component } from 'react'
+
+// Class style
+class TestHook extends Component {
+	state = {
+		name : 'BS',
+		age  : 43
+	}
+	render () {
+		const { age, name } = this.state
+		return (
+			<div>
+				<h1>This is React hooks test</h1>
+				<div>
+					Name :{' '}
+					<input
+						type="text"
+						value={name}
+						onChange={(e) => this.setState({ name: e.target.value.toUpperCase() })}
+					/>
+					<div>My name is : {name} </div>
+				</div>
+				<div> age: {age}</div>
+				<button onClick={(e) => this.setState({ age: age + 1 })} />
+			</div>
+		)
+	}
+}
+export default TestHook
+
+// Hooks Style
 
